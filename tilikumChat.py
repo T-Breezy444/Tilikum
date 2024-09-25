25310c325c01f01409b151e52930b616e480c4a7
Tyler Blalock <122387381+T-Breezy444@users.noreply.github.com>
2024-09-25 10:40:56 -0400

    Update tilikumChat.py
    importing chat loop

--- tilikumChat.py
+++ tilikumChat.py
@@ -1,1 +1,208 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import mmap
+import random
+import pickle
+import argparse
+from transformers import AutoTokenizer
+import time
+
+# from flash_attn import flash_attn_qkvpacked_func, flash_attn_func
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+block_size = 128
+batch_size = 64
+max_iters = 1000
+learning_rate = 3e-4
+eval_iters = 100
+n_embd = 600
+n_layer = 12
+n_head = 12
+dropout = 0.2
+
+print('Using device:', device)
+
+chars = ''
+with open('C:\\Users\\mango\\dev\\Tilikum\\Models\\BigReddit\\RedditVocab.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+    chars = sorted(list(set(text)))
+
+vocab_size = len(chars)
+print('Vocab size:', vocab_size)
+
+# Tokenizer mappings
+string_to_int = {ch: i for i, ch in enumerate(chars)}
+int_to_string = {i: ch for i, ch in enumerate(chars)}
+encode = lambda s: [string_to_int.get(ch, 0) for ch in s]  # Default to 0 if char not found
+decode = lambda x: ''.join([int_to_string.get(i, '?') for i in x])  # Default to '?' if index not found
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters, device=device)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+class Head(nn.Module):
+    """ one head of self-attention """
+
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        # input of size (batch, time-step, channels)
+        # output of size (batch, time-step, head size)
+        B, T, C = x.shape
+        k = self.key(x)   # (B, T, hs)
+        q = self.query(x) # (B, T, hs)
+        # compute attention scores ("affinities")
+        wei = q @ k.transpose(-2,-1) * k.shape[-1]**-0.5 # (B, T, hs) @ (B, hs, T) -> (B, T, T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
+        wei = F.softmax(wei, dim=-1) # (B, T, T)
+        wei = self.dropout(wei)
+        # perform the weighted aggregation of the values
+        v = self.value(x) # (B, T, hs)
+        out = wei @ v # (B, T, hs)
+        return out
+
+class MultiHeadAttention(nn.Module):
+    """ multiple heads of self-attention in parallel """
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(head_size * num_heads, n_embd)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1) # (B, T, F)
+        out = self.dropout(self.proj(out))
+        return out
+
+class FeedForward(nn.Module):
+    """ a simple linear layer followed by a non-linearity """
+
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+class Block(nn.Module):
+    """ Transformer block: communication followed by computation """
+
+    def __init__(self, n_embd, n_head):
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        y = self.sa(x)
+        x = self.ln1(x + y)
+        y = self.ffwd(x)
+        x = self.ln2(x + y)
+        return x
+
+class GPTLanguageModel(nn.Module):
+    def __init__(self, vocab_size):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd) # final layer norm
+        self.lm_head = nn.Linear(n_embd, vocab_size)
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def forward(self, index, targets=None):
+        B, T = index.shape
+
+        # idx and targets are both (B, T) tensor of integers
+        tok_emb = self.token_embedding_table(index) # (B, T, C)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, C)
+        x = tok_emb + pos_emb # (B, T, C)
+        x = self.blocks(x) # (B, T, C)
+        x = self.ln_f(x) # (B, T, C)
+        logits = self.lm_head(x) # (B, T, vocab_size)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B * T, C)
+            targets = targets.view(B * T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, index, max_new_tokens):
+        # index is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # Crop idx to the last block_size tokens
+            index_cond = index[:, -block_size:]
+            # Get the predictions
+            logits, _ = self.forward(index_cond)
+            # Focus only on the last time step
+            logits = logits[:, -1, :] # (B, C)
+            # Apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B, C)
+            # Sample from the distribution
+            index_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # Append sampled index to the running sequence
+            index = torch.cat((index, index_next), dim=1) # (B, T+1)
+        return index
+
+
+
+print('Loading model...')
+with open('C:\\path\\to\\model\\model.pkl', 'rb') as f:
+    model = pickle.load(f)
+print('Model loaded')
+m = model.to(device)
+
+
+while True:
+    prompt = input(f"\nPrompt: \n")
+
+    start_time = time.time()
+    
+    context = torch.tensor(encode(prompt), dtype=torch.long, device=device)
+    response = decode(m.generate(context.unsqueeze(0), 30)[0].tolist())
+    
+    end_time = time.time()
+    total_time = end_time - start_time
+    
+    print(f'\nTilikum: \n {response} \n Time: {total_time} seconds')
 
